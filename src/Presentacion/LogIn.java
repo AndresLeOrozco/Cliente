17518f2b3126076fdcfb41b6fa8fdf14d3afedc9
andre <andres.leon.orozco@est.una.ac.cr>
2022-11-08 09:23:19 -0600

    Initial commit

---
+++ src/Presentacion/LogIn.java
@@ -0,0 +1,57 @@
+package Presentacion;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class LogIn extends JFrame{
+    private JPanel MainPanel;
+    private JTextField UserName;
+    private JTextField Contrasenia;
+    private JButton registerButton;
+    private JButton logInButton;
+    private JPanel logInPanel;
+    private JPanel Register;
+    private JPanel encabezado;
+    private JPanel registerData;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JButton registerButton1;
+    private JPanel logInQuestion;
+    private JButton logInButton1;
+    private JPanel question;
+
+    public LogIn(){
+        add(MainPanel);
+        setLocationRelativeTo(null);
+        setSize(500,200);
+        setVisible(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        registerButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+               MainPanel.removeAll();
+               MainPanel.add(Register);
+               MainPanel.repaint();
+               MainPanel.revalidate();
+            }
+        });
+        logInButton1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                MainPanel.removeAll();
+                MainPanel.add(logInPanel);
+                MainPanel.repaint();
+                MainPanel.revalidate();
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        LogIn lo = new LogIn();
+    }
+
+}
